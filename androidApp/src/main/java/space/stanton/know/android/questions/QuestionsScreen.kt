ea295863411385a04064fbd9443cc1e91b90ea11
Stanton <stantronic@gmail.com>
2025-05-05 15:26:45 +0100

    Add loading state

--- androidApp/src/main/java/space/stanton/know/android/questions/QuestionsScreen.kt
+++ androidApp/src/main/java/space/stanton/know/android/questions/QuestionsScreen.kt
@@ -1,11 +1,19 @@
 package space.stanton.know.android.questions
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.koinViewModel
 import space.stanton.know.android.AppScreen
+import space.stanton.know.android.ui.LargeSpacer
 import space.stanton.know.di.QuestionsViewModel
 
 @Composable
@@ -22,6 +30,20 @@ fun QuestionsScreen(
         LaunchedEffect(score.complete) {
             if (score.complete) {
                 onComplete(score.toJson())
+            }
+        }
+
+        if (question == null) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Fetching...")
+                    LargeSpacer()
+                    CircularProgressIndicator()
+                }
             }
         }
         question?.let {
